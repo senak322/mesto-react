a4dafdf06e6ec928a90194f43774ed1b70318a57
Alexander Vizniy <senak98@yandex.ru>
2022-11-02 02:06:25 +0300

    fix all

--- src/components/AddPlacePopup.js
+++ src/components/AddPlacePopup.js
@@ -1,48 +1,51 @@
 import React from 'react';
 import PopupWithForm from './PopupWithForm.js';
-import {useForm} from '../hooks/useForm.js'
+import { useForm } from '../hooks/useForm.js';
 
-function AddPlacePopup (props) {
-  
-    const {values, handleChange, setValues} = useForm({})
+function AddPlacePopup(props) {
 
-    function handleSubmit(e) {
-        e.preventDefault();
-        debugger
-        props.onAddPlace(
-          values
-        )
-        
-    }
+  const { values, handleChange, setValues } = useForm({})
 
-    return (
+  function handleSubmit(e) {
+    e.preventDefault();
+    props.onAddPlace(
+      values
+    )
+  }
+
+  React.useEffect(() => {
+    setValues({});
+    
+}, [props.isOpen]);
+
+  return (
     <PopupWithForm name={'add'} title={'Новое место'} buttonText={'Создать'} child={<>
-        <input
-          className="popup__input popup__input_type_place"
-          id="place"
-          name="place"
-          type="text"
-          placeholder="Название"
-          required=""
-          minLength={2}
-          maxLength={30}
-          value={values.place || ''}
-          onChange={handleChange}
-        />
-        <span className="popup__error place-error" />
-        <input
-          className="popup__input popup__input_type_link"
-          id="link"
-          name="link"
-          type="url"
-          placeholder="Ссылка на картинку"
-          required=""
-          value={values.link || ''}
-          onChange={handleChange}
-        />
-        <span className="popup__error popup__error_type_active link-error" />
-  
-      </>} isOpen={props.isOpen} onClose={props.onClose} onSubmit={handleSubmit}/>)
+      <input
+        className="popup__input popup__input_type_place"
+        id="place"
+        name="place"
+        type="text"
+        placeholder="Название"
+        required=""
+        minLength={2}
+        maxLength={30}
+        value={values.place || ''}
+        onChange={handleChange}
+      />
+      <span className="popup__error place-error" />
+      <input
+        className="popup__input popup__input_type_link"
+        id="link"
+        name="link"
+        type="url"
+        placeholder="Ссылка на картинку"
+        required=""
+        value={values.link || ''}
+        onChange={handleChange}
+      />
+      <span className="popup__error popup__error_type_active link-error" />
+
+    </>} isOpen={props.isOpen} onClose={props.onClose} onSubmit={handleSubmit} />)
 }
 
 export default AddPlacePopup;
